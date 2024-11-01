34c4e80a7a0e093d79a7d6644dd3b59bdf357704
Yuriy Matrosov <matrosovurij@gmail.com>
2024-11-01 13:50:01 +0300

    java file commit

---
+++ TestFileMain.java
@@ -0,0 +1,8 @@
+
+public class TestFileMain {
+    public static void main(String[] args) {
+        String message = "Hello for Git";
+        for (int i = 0; i < args.length; i++)
+            System.out.println(message.charAt(i));
+    }
+}
