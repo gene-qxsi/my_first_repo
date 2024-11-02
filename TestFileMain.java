6c9be7406e6bcca4e5a0d5806bd5c85e0b723241
Yuriy Matrosov <matrosovurij@gmail.com>
2024-11-02 11:53:01 +0300

    just upd

--- TestFileMain.java
+++
@@ -1,8 +0,0 @@
-
-public class TestFileMain {
-    public static void main(String[] args) {
-        String message = "Hello for Git";
-        for (int i = 0; i < args.length; i++)
-            System.out.println(message.charAt(i));
-    }
-}
