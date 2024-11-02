babb4a750da2d74cc3e3f0a14651faf7e3abe54a
Yuriy Matrosov <matrosovurij@gmail.com>
2024-11-02 09:46:04 +0300

    add class 'MathMethods.java'

---
+++ git-project-test/src/main/java/com/matrosov/MathMethods.java
@@ -0,0 +1,12 @@
+package com.matrosov;
+
+public class MathMethods {
+    public static long plus(long n1, long n2) {
+        return n1 + n2;
+    }
+
+    public static long minus(long bigNum, long minusNum) {
+        return bigNum - minusNum;
+    }
+
+}
